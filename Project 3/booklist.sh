a25f84554425635ef61836cb562ce8768e3a2b46
Dylan Strohschein <strohscd@eos26.cis.gvsu.edu>
2019-03-27 13:50:38 -0400

    Added to proj3

--- Project 3/booklist.sh
+++ Project 3/booklist.sh
@@ -1,3 +1,14 @@
+if [ ! $# -eq 1 ]
+then
+    echo This program takes 1 command line argument
+    echo is should be a list of book information
+fi
+
+if [ ! -r $1 ]
+then
+    echo This program could not read the file $1
+fi
+
 echo Welcome to booklist
 echo -------------------
 echo "(1) Search the booklist for entries by author"
@@ -17,7 +28,8 @@ case $response in
         IFS=':'
         while read line line2 line3 || [ -n "$line" ];
         do 
-            if [[ $search == *"$line"* ]]
+            lowerAuthor=$(echo $line | tr 'A-Z' 'a-z')
+            if [[ "$lowerAuthor" == *"$search"* ]]
             then
                 echo -e $line '\t\t' $line2 '\t\t' $line3
             fi
@@ -25,10 +37,46 @@ case $response in
         OIFS=IFS
     ;;
     2) #Add
+        echo What is the book Author?
+        read author
+        echo What is the book Title?
+        read title
+        echo What is the book Year?
+        read year
 
+        found=0
+        while read line line2 line3 || [ -n "$line" ];
+        do 
+            if [ $author -eq $line ] && 
+            [ $title -eq $line2 ] && [ $year -eq $line3 ]
+            then
+                found=1
+                echo found
+            fi
+        done <$1
+
+        if [ found == 0 ]
+        then
+            echo -e '\n'$author':'$title':'$year >> $1
+        else
+            echo "This entry is already in the file"
+        fi
+        
     ;;
-    3) #Delete
-
+    3) #Delete by title
+        echo What is the title of the book you want to delete:
+        read search
+        OIFS=$IFS
+        IFS=':'
+        while read line line2 line3 || [ -n "$line" ];
+        do 
+            if [[ ! $line2 == $search ]]
+            then
+                echo $line':'$line2':'$line3
+            fi
+        done <$1 >o
+        mv o $1
+        OIFS=IFS
     ;;
     4) #Display
         OIFS=$IFS
@@ -37,7 +85,7 @@ case $response in
         while read line line2 line3 || [ -n "$line" ];
         do 
             count=$((count + 1))
-            echo -e $line '\t\t' $line2 '\t\t' $line3
+            echo -e $line '\t\t\t' $line2 '\t\t\t' $line3
         done < $1
         echo The toal number of books is $count
         OIFS=IFS
