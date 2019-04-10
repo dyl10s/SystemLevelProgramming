b0ee8f9acc6025f1473faf86cfdc6ae952553ddb
Dylan Strohschein <strohscd@eos26.cis.gvsu.edu>
2019-04-10 13:36:10 -0400

    I did it im done

---
+++ Bash/DataOrginization.sh
@@ -0,0 +1,4 @@
+#awk - Unix
+#gawk - GNU
+#Both are programmable
+#gawk [-F separation] 'patern {action}' file(s)
--- Project 3/booklist.sh
+++ Project 3/booklist.sh
@@ -1,96 +1,114 @@
-if [ ! $# -eq 1 ]
-then
-    echo This program takes 1 command line argument
-    echo is should be a list of book information
-fi
+trap 'echo You cant leave like that dog' 2
+trap 'echo You cant leave like that dog' 20
 
-if [ ! -r $1 ]
-then
-    echo This program could not read the file $1
-fi
+while [ true ]
+do
+    if [ ! $# -eq 1 ]
+    then
+        echo This program takes 1 command line argument
+        echo is should be a list of book information
+    fi
 
-echo Welcome to booklist
-echo -------------------
-echo "(1) Search the booklist for entries by author"
-echo "(2) Add a new entry to the booklist"
-echo "(3) Delete an entry from the booklist (using title as the key)"
-echo "(4) Display all entries and the number of entries in the booklist"
-echo "(5) Exit"
+    if [ ! -r $1 ]
+    then
+        echo This program could not read the file $1
+    fi
 
-read response
+    echo "1 Search the booklist for entries by author 
+2 Add a new entry to the booklist
+3 Delete an entry from the booklist (using title as the key)
+4 Display all entries and the number of entries in the booklist
+5 Exit"
 
-case $response in
-    1) #Search
-        echo What is your search:
-        read search
-        search=$(echo $search | tr 'A-Z' 'a-z')
-        OIFS=$IFS
-        IFS=':'
-        while read line line2 line3 || [ -n "$line" ];
-        do 
-            lowerAuthor=$(echo $line | tr 'A-Z' 'a-z')
-            if [[ "$lowerAuthor" == *"$search"* ]]
+    read response
+
+    case $response in
+        1) #Search
+            echo "What is your search?"
+            read search
+            search=$(echo $search | tr 'A-Z' 'a-z')
+            OIFS=$IFS
+            IFS=':'
+            output=""
+            while read line line2 line3 || [ -n "$line" ];
+            do 
+                lowerAuthor=$(echo $line | tr 'A-Z' 'a-z')
+                if [[ "$lowerAuthor" == *"$search"* ]]
+                then
+                    output="$output $line $line2 $line3"
+                    output+=$'\n'
+                fi
+            done < $1
+            echo $output
+            OIFS=IFS
+        ;;
+        2) #Add
+            echo What is the book Author?
+            read author
+            echo What is the book Title?
+            read title
+            echo What is the book Year?
+            read year
+
+            OIFS=$IFS
+            IFS=':'
+            found=0
+            while read line line2 line3 || [ -n "$line" ];
+            do 
+                if [ "$author" = "$line" ] && [ "$title" = "$line2" ] && [ "$year" = "$line3" ]
+                then
+                    found=1
+                fi
+            done <$1
+
+            if [[ found -eq 0 ]]
             then
-                echo -e $line '\t\t' $line2 '\t\t' $line3
+                echo -e $author':'$title':'$year >> $1
+                echo "Add Complete"
+            else
+                echo "This entry is already in the file"
             fi
-        done < $1
-        OIFS=IFS
-    ;;
-    2) #Add
-        echo What is the book Author?
-        read author
-        echo What is the book Title?
-        read title
-        echo What is the book Year?
-        read year
-
-        found=0
-        while read line line2 line3 || [ -n "$line" ];
-        do 
-            if [ $author -eq $line ] && 
-            [ $title -eq $line2 ] && [ $year -eq $line3 ]
+            OIFS=IFS
+        ;;
+        3) #Delete by title
+            echo What is the title of the book you want to delete:
+            read search
+            OIFS=$IFS
+            IFS=':'
+            found=0
+            while read line line2 line3 || [ -n "$line" ];
+            do 
+                if [[ ! $line2 == $search ]]
+                then
+                    echo $line':'$line2':'$line3
+                else
+                    found=1
+                fi
+            done <$1 >o
+            if [[ found -eq 0 ]]
             then
-                found=1
-                echo found
+                echo "Book not found!"
             fi
-        done <$1
-
-        if [ found == 0 ]
-        then
-            echo -e '\n'$author':'$title':'$year >> $1
-        else
-            echo "This entry is already in the file"
-        fi
-        
-    ;;
-    3) #Delete by title
-        echo What is the title of the book you want to delete:
-        read search
-        OIFS=$IFS
-        IFS=':'
-        while read line line2 line3 || [ -n "$line" ];
-        do 
-            if [[ ! $line2 == $search ]]
-            then
-                echo $line':'$line2':'$line3
-            fi
-        done <$1 >o
-        mv o $1
-        OIFS=IFS
-    ;;
-    4) #Display
-        OIFS=$IFS
-        IFS=':'
-        count=0
-        while read line line2 line3 || [ -n "$line" ];
-        do 
-            count=$((count + 1))
-            echo -e $line '\t\t\t' $line2 '\t\t\t' $line3
-        done < $1
-        echo The toal number of books is $count
-        OIFS=IFS
-    ;;
-    5) #Exit
-        exit 0
-    ;;
-esac
+            mv o $1
+            OIFS=IFS
+        ;;
+        4) #Display
+            OIFS=$IFS
+            IFS=':'
+            count=0
+            output=""
+            while read line line2 line3 || [ -n "$line" ];
+            do 
+                count=$((count + 1))
+                output="$output $line $line2 $line3"
+                output+=$'\n'
+            done < $1
+            output="$output The toal number of books is $count"
+            echo $output
+            OIFS=IFS 
+        ;;
+        5) #Exit
+            exit 0
+        ;;
+    esac
+done
